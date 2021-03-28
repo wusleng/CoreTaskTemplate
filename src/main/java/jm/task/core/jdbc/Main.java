dad3be5bdfc636ba5a8630c4fbf5dfbb44d3b5ce
wusleng <67842198+wusleng@users.noreply.github.com>
2021-03-28 22:54:48 +0300

    git checkout git push

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -5,32 +5,64 @@ import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.service.UserService;
 import jm.task.core.jdbc.service.UserServiceImpl;
 import jm.task.core.jdbc.util.Util;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
 
+
+
 public class Main {
 
 
-    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        public static void main(String[] args) throws SQLException, ClassNotFoundException {
 
-        UserService userServ = new UserServiceImpl();
-        userServ.createUsersTable();
-        userServ.saveUser("John", "Nikson", (byte) 56);
-        userServ.saveUser("Sarah", "Konor", (byte) 34);
-        userServ.saveUser("Peter", "Mexes", (byte) 33);
-        userServ.saveUser("Leam", "Howlet", (byte) 51);
+        SessionFactory sf = Util.getSessionFactory();
 
-        System.out.println(userServ.getAllUsers());
+        Session session = sf.openSession();
 
-        userServ.removeUserById(2);
-        userServ.cleanUsersTable();
-        userServ.dropUsersTable();
+        session.getTransaction().begin();
 
-        Util.getConnectionToDatabase().close();
+        User user_1 = new User();
+        user_1.setName("John");
+        user_1.setLastName("Nikson");
+        user_1.setAge((byte) 56);
+
+        User user_2 = new User();
+        user_2.setName("Sarah");
+        user_2.setLastName("Konor");
+        user_2.setAge((byte) 34);
+
+        User user_3 = new User();
+        user_3.setName("Peter");
+        user_3.setLastName("Mexes");
+        user_3.setAge((byte) 33);
+
+        User user_4 = new User();
+        user_4.setName("Leam");
+        user_4.setLastName("Howlet");
+        user_4.setAge((byte) 51);
+
+                //        UserService userServ = new UserServiceImpl();
+//        userServ.createUsersTable();
+//        userServ.saveUser("John", "Nikson", (byte) 56);
+//        userServ.saveUser("Sarah", "Konor", (byte) 34);
+//        userServ.saveUser("Peter", "Mexes", (byte) 33);
+//        userServ.saveUser("Leam", "Howlet", (byte) 51);
+//
+//        System.out.println(userServ.getAllUsers());
+//
+//        userServ.removeUserById(2);
+//        userServ.cleanUsersTable();
+//        userServ.dropUsersTable();
+//
+//        Util.getConnectionToDatabase().close();
+
     }
+
 }
 
 
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,19 +1,76 @@
 package jm.task.core.jdbc.util;
 
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import org.hibernate.service.ServiceRegistry;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.sql.Statement;
+import java.util.Properties;
 
 public class Util {
     public static Connection getConnectionToDatabase() throws ClassNotFoundException, SQLException {
-
-        final String DB_URL = "jdbc:mysql://localhost:3306/mydb?serverTimezone=UTC";
-        final String DB_Driver = "java.sql.Driver";
+        //Connect in different ways - коннект разными способами
+        final String DB_URL = "jdbc:mysql://localhost:3306/mydb?serverTimezone=UTC"; // connect with jdbc
+        final  String DB_Driver = "java.sql.Driver";
 
         Connection connection = DriverManager.getConnection(DB_URL, "root", "Wusleng567706"); //соединениесБД
         System.out.println("Соединение с СУБД выполнено.");
 
         return connection;
+
     }
-}
+        private static SessionFactory sessionFactory;
+        private static ServiceRegistry serviceRegistry;
+        // Hibernate connection
+        public static SessionFactory getSessionFactory () {
+            if (sessionFactory == null) {
+                try {
+                    // Hibernate settings equivalent to hibernate.cfg.xml's properties
+                    Properties properties = new Properties();
+                    properties.put(Environment.DRIVER, "java.sql.Driver");
+                    properties.put(Environment.URL, "jdbc:mysql://localhost:3306/mydb?serverTimezone=UTC");
+                    properties.put(Environment.USER, "root");
+                    properties.put(Environment.PASS, "Wusleng567706");
+                    properties.put(Environment.DIALECT, "org.hibernate.dialect.MySQL5Dialect");
+                    properties.put(Environment.SHOW_SQL, true);
+                    properties.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+                    properties.put(Environment.ORDER_UPDATES, "true");
+                    properties.put(Environment.HBM2DDL_AUTO, "create-drop");
+
+                    Configuration configuration = new Configuration();
+                    configuration.setProperties(properties);
+                    configuration.addAnnotatedClass(Util.class);
+                    
+                    serviceRegistry = new StandardServiceRegistryBuilder()
+                            .applySettings(configuration.getProperties())
+                            .build();
+                    sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+                    System.out.println("SessionFactory - создан");
+                } catch (Exception e) {
+                    System.out.println("SessionFactory - НЕ создан");
+                    e.printStackTrace();
+                    StandardServiceRegistryBuilder.destroy(serviceRegistry);
+                }
+            }
+            return sessionFactory;
+        }
+//    public static SessionFactory buildSessionFactory(){
+//        try {
+//            StandardServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+//                    .build();               // Сервис через который идет управление
+//
+//            Metadata metadata = new MetadataSources(serviceRegistry).getMetadataBuilder().build();
+//                                                                            // метаданные для построения сешн фактори
+//            return metadata.getSessionFactoryBuilder().build();
+//        }catch (Throwable ex){
+//            System.err.println("Initial session Factory creation failed");
+//            throw new ExceptionInInitializerError(ex);
+//        }
+//    }
+    }
+
